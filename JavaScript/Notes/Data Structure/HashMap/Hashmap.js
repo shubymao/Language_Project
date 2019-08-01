161d1dc706ea0471318c876a6adfa116ab72e231
Shuby_Mao <le-li.mao@mail.mcgill.ca>
2019-08-01 01:48:46 -0400

    added hashmaps

---
+++ JavaScript/Notes/Data Structure/HashMap/Hashmap.js
@@ -0,0 +1,11 @@
+let mp = {}
+mp[1]="ONE"
+mp[2]="TWO"
+mp[0]="Zero"
+console.log(mp)
+// let http = require('http')
+// http.createServer(function(req,res){
+//     res.writeHead(200,{'content-type':'text/plain'})
+//     res.end("Hello World")
+// }).listen(9001)
+
